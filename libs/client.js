554aeac063829034641e532b5f09921b5ea7afc1
n0aaaeth <n0aaaeth@gmail.com>
2022-12-03 20:18:33 +0900

    all done

---
+++ libs/client.js
@@ -0,0 +1,7 @@
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+    serviceDomain: "noa-blog-tutorial",
+    apiKey: "20a8fed464c7426bae5bbfd6dbf6420caf51"
+});
+
